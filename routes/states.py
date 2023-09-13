b865826e11ffcc4f047949fd7433dc3db832d8d8
Daminov Kamil <kamil@MacBook-Pro-Daminov.local>
2023-09-13 11:14:09 +0500

    Another one bug

--- routes/states.py
+++ routes/states.py
@@ -12,27 +12,6 @@ import config
 bot = Bot(config.bot_token, parse_mode=ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
 
 
-async def add_message(message: Message, state: FSMContext) -> None:
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    data = await state.get_data()
-    if "messages" not in data:
-        data["messages"] = []
-    data["messages"].append(message)
-    await state.set_data(data)
-
-
-async def delete_messages(state: FSMContext) -> None:
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    data = await state.get_data()
-    if "messages" in data:
-        for message in data["messages"]:
-            await message.delete()
-
-
 async def try_edit_msg(message: Message, *args, **kwargs):
     try:
         await message.edit_text(*args, **kwargs)
@@ -59,3 +38,24 @@ async def try_bot_edit_msg_markup(*args, **kwargs):
         await bot.edit_message_reply_markup(*args, **kwargs)
     except Exception as error:
         logging.debug(error)
+
+
+async def add_message(message: Message, state: FSMContext) -> None:
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    data = await state.get_data()
+    if "messages" not in data:
+        data["messages"] = []
+    data["messages"].append(message)
+    await state.set_data(data)
+
+
+async def delete_messages(state: FSMContext) -> None:
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    data = await state.get_data()
+    if "messages" in data:
+        for message in data["messages"]:
+            await try_delete_msg(message)
